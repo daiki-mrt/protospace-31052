22e49cff84a32683df5fbb5f0e32f86d3c3eda28
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-10-09 19:59:42 +0900

    アクセス制限修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show] 
   before_action :move_to_index, only: :edit
 
   def index
@@ -50,7 +51,8 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user.id
       redirect_to root_path
     end
   end  
